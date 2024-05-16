d0e34ef7b976eefa8221a0635f1fab4452679f26
Ge932004 <157493578+Ge932004@users.noreply.github.com>
2024-05-16 22:38:10 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,34 @@
+# Title
+st.header("Streamlit Machine Learning App")
+
+# Input bar 1
+SE_right = st.number_input("SE_right")
+
+# Input bar 2
+Age_group = st.number_input("Age_group")
+
+
+# Input bar 3
+School = st.number_input("School")
+
+# Input bar 4
+UDVA_group = st.number_input("UDVA_group")
+
+# Input bar 4
+Vision_correction = st.number_input("Vision_correction")
+
+# If button is pressed
+if st.button("Submit"):
+    
+    # Unpickle classifier
+    clf = joblib.load("clf.pkl")
+    
+    # Store inputs into dataframe
+    X = pd.DataFrame([[SE_right, Age_group, School, UDVA_group, Vision_correction]], 
+                     columns = ["SE_right", "Age_group", "School","UDVA_group","Vision_correction"])
+    
+    # Get prediction
+    prediction = clf.predict(X)[0]
+    
+    # Output prediction
+    st.text(f"This instance is a {prediction}")
